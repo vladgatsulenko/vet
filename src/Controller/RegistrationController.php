829156dabca9c4c8959b7cce362cb84100eeb57c
vlad <vladgatsulenko@gmail.com>
2025-07-28 23:10:46 +0300

    Translated all hardcoded text

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -9,9 +9,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\UserService;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
 {
+
+    public function __construct(private TranslatorInterface $translator)
+    {
+        
+    }
+
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserService $userService): Response
     {
@@ -23,7 +30,11 @@ class RegistrationController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $userService->registerUser($user, $user->getPassword());
 
-            $this->addFlash('success', 'Для завершения регистрации подтвердите ваш Email. Проверьте вашу почту для подтверждения.');
+            $this->addFlash(
+                'success',
+                $this->translator->trans('flashRegistrationEmailSent', [], 'security')
+            );
+
             return $this->redirectToRoute('app_register');
         }
 
