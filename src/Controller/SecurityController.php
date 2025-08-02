8ef782b21ed938833554826dadd6dfe202a4c074
vlad <vladgatsulenko@gmail.com>
2025-08-02 20:18:02 +0300

    add Makefile and commands

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -24,6 +24,6 @@ class SecurityController extends AbstractController
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
-        throw new \LogicException();
+        throw new \LogicException('This method can be blank-it will be intercepted by the logout key on your firewall');
     }
 }
