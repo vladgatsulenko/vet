d08d6d70cbc64d7be23bd1250d59e1401c7ccf65
vlad <vladgatsulenko@gmail.com>
2025-08-08 12:59:09 +0300

    add language button

---
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    private string $defaultLocale;
+
+    public function __construct(string $defaultLocale = 'ru')
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        $locale = $request->getSession()->get('_locale', $this->defaultLocale);
+        $request->setLocale($locale);
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::REQUEST => [['onKernelRequest', 20]],
+        ];
+    }
+}
